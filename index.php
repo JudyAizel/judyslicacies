ba403ead2b4a4f254e7be87ea7480a9a50c04a8f
Judy Aizel Utayde <60762908+JudyAizel@users.noreply.github.com>
2020-12-07 00:11:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>LittleBamboo - Judy's Delicacies</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/index.css" />
+  </head>
+  <body>
+    <div class="content">
+      <nav class="navbar">
+        <div class="social-media">
+          <a href="#" class="nav-link">
+            <img src="https://img.icons8.com/ios/50/000000/facebook-new.png" />
+          </a>
+          <a href="#" class="nav-link">
+            <img src="https://img.icons8.com/ios/50/000000/instagram-new.png" />
+          </a>
+          <a href="#" class="nav-link">
+            <img src="https://img.icons8.com/ios/50/000000/twitter.png" />
+          </a>
+        </div>
+        <div class="brand">Little Bamboo</div>
+        <div class="searchbar">
+          <input type="text" placeholder="Search" />
+          <a class="btn-search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="50"
+              height="50"
+              viewBox="0 0 172 172"
+              style="fill: #000000"
+            >
+              <g
+                fill="none"
+                fill-rule="nonzero"
+                stroke="none"
+                stroke-width="1"
+                stroke-linecap="butt"
+                stroke-linejoin="miter"
+                stroke-miterlimit="10"
+                stroke-dasharray=""
+                stroke-dashoffset="0"
+                font-family="none"
+                font-weight="none"
+                font-size="none"
+                text-anchor="none"
+                style="mix-blend-mode: normal"
+              >
+                <path d="M0,172v-172h172v172z" fill="none"></path>
+                <g fill="#ffffff">
+                  <path
+                    d="M72.24,10.32c-32.26344,0 -58.48,26.21656 -58.48,58.48c0,32.26344 26.21656,58.48 58.48,58.48c12.76563,0 24.56375,-4.11187 34.185,-11.0725l45.2575,45.15l9.675,-9.675l-44.72,-44.8275c8.78813,-10.23937 14.0825,-23.52906 14.0825,-38.055c0,-32.26344 -26.21656,-58.48 -58.48,-58.48zM72.24,17.2c28.54125,0 51.6,23.05875 51.6,51.6c0,28.54125 -23.05875,51.6 -51.6,51.6c-28.54125,0 -51.6,-23.05875 -51.6,-51.6c0,-28.54125 23.05875,-51.6 51.6,-51.6z"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </body>
+</html>
